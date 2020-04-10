c54b6ea3395edbb3a23be901f3c7eff97261ab30
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-10 16:29:51 +0100

    Update senderTransactionBC1.java

--- Networking/userSide/senderTransactionBC1.java
+++ Networking/userSide/senderTransactionBC1.java
@@ -2,7 +2,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileExample1 {
+public class senderTransactionBC1 {
 
     public static void main(String[] args) {
 
