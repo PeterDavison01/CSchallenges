b41bedbe58b00c957289a1109e7cff941c59725c
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-18 19:48:09 +0100

    Update CreateNewObject.js

--- Networking/rpiSide/static/CreateNewObject.js
+++ Networking/rpiSide/static/CreateNewObject.js
@@ -2,6 +2,6 @@ function createNewObject() {//, ArrayList<String ) {
 	var name = document.getElementById("createName");
 	var obj = new ObjectConstructor();
 	obj.setName(name);
-	obj.setMarkerNum(numOfObjs);
 	numOfObjs++;
+	obj.setMarkerNum(numOfObjs++;);
 }
