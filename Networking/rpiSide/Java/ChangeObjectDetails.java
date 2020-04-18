79253a1dfd8d7e863c2bcb0d6ec10fa77911d770
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-18 16:45:51 +0100

    Delete ChangeObjectDetails.java

--- Networking/rpiSide/Java/ChangeObjectDetails.java
+++
@@ -1,9 +0,0 @@
-package rpiSide;
-
-public class ChangeObjectDetails {
-
-	public void changeObjectDetails(ObjectDetails object, String newName, String newObjCharac) {		
-		object.setName(newName);
-		object.objCharacs.add(newObjCharac);
-	}
-}
