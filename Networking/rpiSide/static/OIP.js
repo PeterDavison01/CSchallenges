10fa4161ab435af27230a9603210a35400060727
PeterDavison01 <peter@davison.me.uk>
2020-04-18 17:01:20 +0100

    Updating from Rpi

--- Networking/rpiSide/static/OIP.js
+++ Networking/rpiSide/static/OIP.js
@@ -27,25 +27,4 @@ function returnObjInfo(){
 	return rtn;
 }
 
-function showMenu(){
-	document.getElementById("menu").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-	if (!event.target.matches('.menubtn')) {
-		var dropdowns = document.getElementsByClassName("menu-dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
-
-
-function createNewObjectUI(){
-
-}
 
