ed622901093a2bcc4b3d485a8effb523d177a0f0
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-18 22:53:30 +0100

    Update Object.js

--- Networking/rpiSide/static/Object.js
+++ Networking/rpiSide/static/Object.js
@@ -1,4 +1,14 @@
-var allObjs = [""];
+var allObjs = [{name: "", markerNum: 1, objCharacs: []}, 
+	      {name: "", markerNum: 2, objCharacs: []},
+	      {name: "", markerNum: 3, objCharacs: []},
+	      {name: "", markerNum: 4, objCharacs: []},
+	      {name: "", markerNum: 5, objCharacs: []},
+	      {name: "", markerNum: 6, objCharacs: []},
+	      {name: "", markerNum: 7, objCharacs: []},
+	      {name: "", markerNum: 8, objCharacs: []},
+	      {name: "", markerNum: 9, objCharacs: []},
+	      {name: "", markerNum: 10, objCharacs: []}];
+
 var i;
 for (i = 1; i < 11; i++){
 	var cookieName = "Object" + i;
@@ -54,7 +64,7 @@ function ObjectConstructor(consName, consMarkerNum, consObjCharacs){
 	this.name = consName;
 	this.markerNum = consMarkerNum;
 	this.objCharacs = consObjCharacs;
-	allObjs[consMarkerNum] = this;
+	allObjs[(consMarkerNum-1)] = this;
 	var objJsonString = JSON.stringify(this);
 	cookieName = "Object" + this.markerNum;
 	setCookie(cookieName, objJsonString, 7);
