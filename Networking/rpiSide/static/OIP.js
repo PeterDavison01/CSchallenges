a4cffc5ac917707115aaa4504ae881a00e2dd0cd
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-18 17:45:46 +0100

    Update OIP.js

--- Networking/rpiSide/static/OIP.js
+++ Networking/rpiSide/static/OIP.js
@@ -31,19 +31,6 @@ function showMenu(){
 	document.getElementById("menu").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-	if (!event.target.matches('.menubtn')) {
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
-
 function openCreateObjForm() {
   document.getElementById("createObj").style.display = "block";
 }
