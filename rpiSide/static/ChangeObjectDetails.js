0b71bf1746a8a8f172f73676dbd9bc80b2c68cbb
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-19 14:33:02 +0100

    Update ChangeObjectDetails.js

--- rpiSide/static/ChangeObjectDetails.js
+++ rpiSide/static/ChangeObjectDetails.js
@@ -5,33 +5,38 @@ function changeObjectDetails() {
 	}
 	else{
 		var chosenObj = searchForObj(chosenObjID);
-		var objNewName = document.getElementById("changingName").value;
-		console.log(objNewName);
-		if (objNewName==""){
-			objNewName = chosenObj.name;
-		}
-		var chosenObjNewDetailsRaw = document.getElementById("changingDetails").value;
-		var chosenObjNewDetails = [];
-		var startingIndex = 0;
-		var endingIndex = 0;
-		var objCharacsIndex = 0;
-		if (chosenObjNewDetailsRaw==""){
-			chosenObjNewDetails.concat(chosenObj.objCharacs);
+		if (chosenObj.name == "){
+	    		window.alert("You must make an object for that marker first!");
 		}
 		else{
-			for(i = 0; i < chosenObjNewDetailsRaw.length; i++){
-				if(chosenObjNewDetailsRaw[i] == "."){
-					endingIndex = i;
-					chosenObjNewDetails[objCharacsIndex] = chosenObjNewDetailsRaw.slice(startingIndex, endingIndex);
-					objCharacsIndex++;
-					endingIndex += 2;
-					startingIndex = endingIndex;
+			var objNewName = document.getElementById("changingName").value;
+			console.log(objNewName);
+			if (objNewName==""){
+				objNewName = chosenObj.name;
+			}
+			var chosenObjNewDetailsRaw = document.getElementById("changingDetails").value;
+			var chosenObjNewDetails = [];
+			var startingIndex = 0;
+			var endingIndex = 0;
+			var objCharacsIndex = 0;
+			if (chosenObjNewDetailsRaw==""){
+				chosenObjNewDetails.concat(chosenObj.objCharacs);
+			}
+			else{
+				for(i = 0; i < chosenObjNewDetailsRaw.length; i++){
+					if(chosenObjNewDetailsRaw[i] == "."){
+						endingIndex = i;
+						chosenObjNewDetails[objCharacsIndex] = chosenObjNewDetailsRaw.slice(startingIndex, endingIndex);
+						objCharacsIndex++;
+						endingIndex += 2;
+						startingIndex = endingIndex;
+					}
 				}
 			}
+			console.log(chosenObjNewDetails);
+			console.log(objNewName);
+			var testObj = new ObjectConstructor(objNewName, chosenObjID, chosenObjNewDetails);
+			console.log(testObj);
 		}
-		console.log(chosenObjNewDetails);
-		console.log(objNewName);
-		var testObj = new ObjectConstructor(objNewName, chosenObjID, chosenObjNewDetails);
-		console.log(testObj);
 	}
 }
