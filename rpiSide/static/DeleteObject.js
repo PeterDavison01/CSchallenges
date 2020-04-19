cb868af39fb0cadfa5a18c1e124f5762bf1d9b53
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-19 14:29:27 +0100

    Update DeleteObject.js

--- rpiSide/static/DeleteObject.js
+++ rpiSide/static/DeleteObject.js
@@ -1,5 +1,5 @@
 function deleteObject(){
-        var chosenObjID = document.getElementById("changingObjID").value;
+        var chosenObjID = document.getElementById("deleteObjID").value;
 	if (1 < chosenObjID || chosenObjID > 10){
 		window.alert("That's not a valid Object Marker Number!");
 	}
