faba260035df328f1f63484ad6e58353f99cc18a
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-19 11:34:57 +0100

    Update ChangeObjectDetails.js

--- rpiSide/static/ChangeObjectDetails.js
+++ rpiSide/static/ChangeObjectDetails.js
@@ -1,4 +1,12 @@
-function changeObjectDetails(ObjectDetails object, String newName, String newObjCharac) {
+function debugChangeObjectDetails(newName,  ObjID, newObjCharacs){
+	var foundObj = searchForObj(ObjID)
+	foundObj.name = newName;
+	var i;
+	for(i = 0; i < foundObj.objCharacs.length; i++){
+		foundObj.objCharacs[i] = newObjCharacs[i]
+}
+
+function changeObjectDetails() {
 	object.setName(newName);
 	object.objCharacs.push(newObjCharac);
 }
