9b8810ac3d8e01a9ff0b11627ee7ef75fe40d98a
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2020-04-20 18:39:10 +0100

    Update OIP.js

--- sourceCode/static/OIP.js
+++ sourceCode/static/OIP.js
@@ -1,6 +1,9 @@
+//Reurns the objects characsteristic in the form of a string.
 function rtnObjCharacs(obj){
+	//Initialising the string that will be returned
 	var rtn = "\n\n";
 	if (obj.objCharacs.length == 0){
+		//Used for when the user has not yet given the object characteristics
 		rtn += "There are currently no object characteristics.";
 	}
 	else{
@@ -13,6 +16,8 @@ function rtnObjCharacs(obj){
 	return rtn;
 }
 
+//Returns the object assigned to that ObjID a.k.a their MarkerNum (as the
+//marker number is unique to each object)
 function searchForObj(objID){
 	for (obj of allObjs){
 		if (obj.markerNum == objID){
@@ -22,6 +27,8 @@ function searchForObj(objID){
 	}
 }
 
+//Uses searchForObj to return all the information about the given object
+//in the form of a string.
 function returnObjInfo(objID){
 	var rtn = "Error loading Object.";
 	var obj = searchForObj(objID);
@@ -32,22 +39,27 @@ function returnObjInfo(objID){
 	return rtn;
 }
 
+//Used to show the menu on the AR page.
 function showMenu(){
 	document.getElementById("menu").classList.toggle("show");
 }
 
+//Used to return to the AR page.
 function closeForm() {
 	window.location.href = "/";
 }
 
+//Used to open the Create new object form
 function openCreateObjForm() {
  	window.location.href = "createNewObject";
 }
 
+//Used to open the Change object details form
 function openChangeObjForm(){
 	window.location.href = "changeObjectDetails";
 }
 
+//Used to open the delete object form
 function openDeleteObjForm(){
 	window.location.href = "deleteObject";
 }
